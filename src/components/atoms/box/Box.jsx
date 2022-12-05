dca61ed35c354a862cb0fbb3a2e7369531827e79
A. Simon <adryel@live.it>
2022-12-05 17:30:14 -0300

    first commit

---
+++ src/components/atoms/box/Box.jsx
@@ -0,0 +1,46 @@
+import * as S from "./styled";
+
+const Box = ({
+    bg,  
+    width,
+    justify,
+    align,
+    height,
+    direction,
+    shadow,
+    color,
+    margin,
+    padding,
+    children,
+    position,
+    radius,
+    gap,
+    textAlign,
+    flexWrap,
+    maxWidth,
+}) => {
+    return (
+        <S.Box
+            padding={padding}
+            margin={margin}
+            color={color}
+            bg={bg}
+            width={width}
+            height={height}
+            justify={justify}
+            align={align}
+            direction={direction}
+            shadow={shadow}
+            position={position}
+            gap={gap}
+            radius={radius}
+            textAlign={textAlign}
+            flexWrap={flexWrap}
+            maxWidth={maxWidth}
+        >
+            {children}
+        </S.Box>
+    );
+};
+
+export default Box;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,47 @@
+import { Container, Nav, Navbar, Offcanvas, Stack } from 'react-bootstrap';
+import Image from "next/image";
+import Logo from '../../../public/static/images/logo.png'
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <>
+      <container className="styles-header">
+        {['xl'].map((expand) => (
+          <Navbar key={expand} expand={expand} className="mb-3">
+            <Container fluid>
+              <div className='logo-header' >
+                <Link href="/">
+                  <Image style={{ cursor: 'pointer'}} src={Logo} alt="logo-header" width="270%" height="100%" />
+                </Link>
+              </div>
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    MENU
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav id='buttons-header' className="justify-content-center  flex-grow-1 pe-3" >
+                    <Nav.Link href="/quemsomos">Quem Somos</Nav.Link>
+                    <Nav.Link href="/programa">O Programa</Nav.Link>
+                    <Nav.Link href="/voluntarios">Voluntários</Nav.Link>
+                    <Nav.Link href="/doacoes">Doações</Nav.Link>
+                    <Nav.Link href="/parcerias">Parcerias</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </container>
+    </>
+  );
+}
+
+export default Header;
